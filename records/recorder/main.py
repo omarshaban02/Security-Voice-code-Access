872b89bf06af4a32fb7b6eb44427c2181e1984b5
omarnasser0 <omar.mohammed021@eng-st.cu.edu.eg>
2023-12-31 06:52:52 +0200

    recorder add to records folder

---
+++ records/recorder/main.py
@@ -0,0 +1,5 @@
+from recorder import record_audio
+
+record_audio(4, "<Your name>_open<no 0 - 40>.wav")
+# record_audio(4, "<Your name>_unlock<no 0 - 40>.wav")
+# record_audio(4, "<Your name>_grant<no 0 - 40>.wav")
