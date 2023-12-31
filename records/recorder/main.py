4ee41360d9921bb01f0026f1af2079f6f894feb2
omarnasser0 <omar.mohammed021@eng-st.cu.edu.eg>
2023-12-31 07:03:35 +0200

    recorder function add to records folder

--- records/recorder/main.py
+++ records/recorder/main.py
@@ -1,5 +1,16 @@
 from recorder import record_audio
 
-record_audio(4, "<Your name>_open<no 0 - 40>.wav")
-# record_audio(4, "<Your name>_unlock<no 0 - 40>.wav")
-# record_audio(4, "<Your name>_grant<no 0 - 40>.wav")
+# Replace <Your name> with your name from this list: [
+# Omar
+# Abdulrahman
+# Abdullah
+# Ahmed
+# ]
+
+# Replace <no 1 - 40> with the number of the record
+
+# Don't forget change this number after each record
+
+record_audio(4, "./<Your name>/open_middle_door/<Your name>_open<no 1 - 40>.wav")
+# record_audio(4, "./<Your name>/grant_me_access/<Your name>_grant<no 1 - 40>.wav")
+# record_audio(4, "./<Your name>/unlock_the_gate/<Your name>_unlock<no 1 - 40>.wav")
