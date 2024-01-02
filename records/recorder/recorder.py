851c00d1a9d19c5dff7002b309638d0a033d97c8
ASG <abdulrahman.ghitani02@eng-st.cu.edu.eg>
2024-01-02 13:59:30 +0200

    ASG records

--- records/recorder/recorder.py
+++ records/recorder/recorder.py
@@ -7,6 +7,7 @@ def record_audio(duration=10, filename="recording.wav"):
   Records audio for `duration` seconds and saves it to `filename`.
   """
     fs = 44100  # Sampling rate
+    print("start")
     recording = sd.rec(duration * fs, samplerate=fs, channels=2)
     sd.wait()
     wv.write(filename, recording, fs, sampwidth=2)
